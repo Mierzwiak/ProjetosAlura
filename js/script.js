fc9bb66df9509d7665d06d4b6303a4a8f3460224
Jonathan <cout.campos@gmail.com>
2023-05-22 23:10:21 -0300

    Primeiro commit solo.

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// script.js
+// Exemplo de funcionalidade para adicionar um item ao carrinho de compras
+
+// Seleciona o botão de adicionar ao carrinho
+var addToCartButton = document.getElementById('add-to-cart');
+
+// Adiciona um ouvinte de evento de clique no botão
+addToCartButton.addEventListener('click', function() {
+  // Lógica para adicionar o item ao carrinho
+  // ...
+});
