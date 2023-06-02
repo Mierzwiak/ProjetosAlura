199d194772b0a8bef27bfdb1b5159faa0e716238
Jonathan <cout.campos@gmail.com>
2023-06-02 00:50:51 -0300

    Ajustando o botão de pesquisar.

--- js/script.js
+++ js/script.js
@@ -1,12 +1,21 @@
 const lupaButton = document.querySelector("#lupaButton");
-const lupaButtonClicado = document.createElement("input"); lupaButtonClicado.type = "text"; lupaButtonClicado.id = "lupaButtonClicado"; lupaButtonClicado.className = "botoesMenuEsquerda";
+{ /* Criação do elemento lupaButtonClicado */
+  var lupaButtonClicado = document.createElement("input"); 
+  lupaButtonClicado.type = "text"; 
+  lupaButtonClicado.id = "lupaButtonClicado"; 
+  lupaButtonClicado.className = "botoesMenuEsquerda";
+  lupaButtonClicado.placeholder = "pesquisar";
+  lupaButtonClicado.style.border = "none";
+}
 
 document.addEventListener("click", (event) => {
 
   if (event.target.id == "lupaButton") {
-    lupaButton.parentNode.replaceChild(lupaButtonClicado, lupaButton)
-  } else if (document.querySelector("#lupaButtonClicado")){
-    lupaButtonClicado.parentNode.replaceChild(lupaButton, lupaButtonClicado)
+    lupaButton.insertAdjacentElement("afterend", lupaButtonClicado);
+    lupaButton.classList.add("botaoAtivo");
+  } else if (document.querySelector("#lupaButtonClicado") && event.target.id != "lupaButtonClicado"){
+    lupaButtonClicado.parentNode.removeChild(lupaButtonClicado);
+    lupaButton.classList.remove("botaoAtivo");
   }
 
 })
