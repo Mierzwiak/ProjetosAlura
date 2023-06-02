a96b00bc3e27f49650fa5348192a58fc88886be3
Leonardo Bulcão <leobc009@gmail.com>
2023-06-01 23:41:56 -0300

    commit

--- js/script.js
+++ js/script.js
@@ -1,12 +1,87 @@
-function clicarPesquisar(params) {
-  
-  var lupaButton = document.querySelector("#lupaButton");
-  var lupaButtonClicado = document.createElement("input");
+var lupaButton = document.querySelector("#lupaButton");
+ 
+var lupaButtonClicado = document.createElement("input");  
 
+
+function clicarPesquisar(params) {
+
+  
   lupaButtonClicado.type = "text";
   lupaButtonClicado.id = "lupaButtonClicado";
   lupaButtonClicado.className = "botoesMenuEsquerda";
 
   lupaButton.parentNode.replaceChild(lupaButtonClicado, lupaButton);
+  console.log("oi")
+  
+}
+
+function Reverter(){
+  // var lupaButtonClicado = document.querySelector("#lupaButtonClicado");
+  var lupaButton = document.createElement("button");
+
+  lupaButton.type = "text";
+  lupaButton.id = "lupaButton";
+  lupaButton.className = "botoesMenuEsquerda";
+  lupaButton.onclick = ()=> {
+    clicarPesquisar()
+  }
+  
+  lupaButtonClicado.parentNode.replaceChild(lupaButton, lupaButtonClicado)
+  console.log("oi2")
 
 }
+
+document.addEventListener("click", (event) => { 
+  // var lupaButtonClicado = document.querySelector("#lupaButtonClicado");
+  console.log(event.target)
+  console.log(lupaButtonClicado)
+  
+  if (event.target !== lupaButton && event.target.id !== "lupaButton") {
+    Reverter()
+
+  }
+ });
+
+
+
+
+
+
+
+// // Adicionar ouvinte de evento para detectar cliques fora do input
+// document.addEventListener("click", verificarCliqueExterno);
+
+
+// // Função para reverter a transformação e voltar para o botão
+// function reverterTransformacao() {
+//   // Selecionar o elemento do input
+//   var input = document.getElementById("meuInput");
+
+//   // Criar um novo elemento de botão
+//   var botao = document.createElement("button");
+//   botao.id = "meuBotao";
+//   botao.style.width = "30px";
+//   botao.style.height = "30px";
+
+//   // Substituir o input pelo botão
+//   input.parentNode.replaceChild(botao, input);
+
+//   // Remover o ouvinte de evento para cliques externos
+//   document.removeEventListener("click", verificarCliqueExterno);
+// }
+
+// // Função para verificar cliques fora do input
+// function verificarCliqueExterno(event) {
+//   var input = document.getElementById("meuInput");
+
+//   // Verificar se o clique ocorreu fora do input e do botão
+//   if (event.target !== input && event.target.id !== "meuBotao") {
+//     reverterTransformacao();
+//   }
+// }
+
+// // Chamar a função para transformar o botão em input quando for clicado
+// function cliqueNoBotao() {
+//   transformarEmInput();
+// }
+
